4ca5d3ffd36204d67532354d776e61601d0509e9
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-10 02:29:50 -0500

    Update Float65.jl

--- src/type/Float65.jl
+++ src/type/Float65.jl
@@ -1,6 +1,7 @@
 bitstype 64 Float65{A} <: AbstractFloat
 
 param{A}(::Type{Float65{A}}) = A 
+param{A}(fp::Float65{A}) = A
 
 Float65(x::Float64) = reinterpret(Float65{zero(Int32)},x)
 Float65(A::Int32, x::Float64) = reinterpret(Float65{A},x)
