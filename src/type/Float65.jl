7abaf4b922adec87aaaefc6718480181c7e737f6
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-10 02:13:30 -0500

    Update Float65.jl

--- src/type/Float65.jl
+++ src/type/Float65.jl
@@ -4,7 +4,7 @@ Float65(x::Float64) = reinterpret(Float65{zero(Int32)},x)
 Float65(A::Int32, x::Float64) = reinterpret(Float65{A},x)
 
 function show(io::IO, x::Float65)
-   a  = typeof(x).parameters[1]
+   A  = typeof(x).parameters[1]
    fp = reinterpret(Float64,x)
    s  = string(fp,"₍",A,"₎")
    print(io, s)
