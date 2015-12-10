958634fa083695525b08b48b711d0360c9453eb3
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-10 02:10:35 -0500

    Update FLOAT65.jl

--- src/FLOAT65.jl
+++ src/FLOAT65.jl
@@ -1,6 +1,10 @@
 module FLOAT65
 
-
+import Base: convert, promote_rule, show, showcompact,
+             isinf, isnan, issubnormal, isfinite,
+             zero, one
+             
+             
 include("Float65.jl")
 include("basics.jl")
 include("fn1arg.jl")
