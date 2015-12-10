32c5c20e294f80d353bfc3182da6eef5d5d594e9
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-10 02:17:22 -0500

    Update Float65.jl

--- src/type/Float65.jl
+++ src/type/Float65.jl
@@ -2,11 +2,8 @@ bitstype 64 Float65{A} <: AbstractFloat
 
 Float65(x::Float64) = reinterpret(Float65{zero(Int32)},x)
 Float65(A::Int32, x::Float64) = reinterpret(Float65{A},x)
+Float65(A::Int64, x::Float64) = Float65(convert(Int32,A),x)
 
 function show(io::IO, x::Float65)
-   A  = typeof(x).parameters[1]
-   fp = reinterpret(Float64,x)
-   s  = string(fp)
-   print(io, s)
-   A
+   show(io, reinterpret(Float64,x))
 end
