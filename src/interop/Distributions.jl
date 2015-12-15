f2a5772fc6e956be2585c7b05671f89857b26f43
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-15 18:48:38 -0500

    Update Distributions.jl

--- src/interop/Distributions.jl
+++ src/interop/Distributions.jl
@@ -6,7 +6,7 @@ import Distributions: Distribution,Univariate,Continuous,Triweight,
                       cdf,pdf,quantile,logcdf,logpdf,ccdf,cquantile,invlogcdf,invlogccdf
 
 typealias TriDist Triweight
-typealias UniDist Distribution{Univariate,Continuous}
+typealias UniDist Distribution{Univariate,Continuous,Distribution}
 
 for op in (:cdf,:pdf,:quantile,:logcdf,:logpdf,:ccdf,:cquantile,:invlogcdf,:invlogccdf)
     @eval ($op)(d::TriDist, x::Float65) = ($op)(d, convert(Float64,x))
