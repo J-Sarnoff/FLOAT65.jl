51797169db8fe6c1078631ab8b695e5cb31946be
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-15 16:01:18 -0500

    Update FLOAT65.jl

--- src/FLOAT65.jl
+++ src/FLOAT65.jl
@@ -14,6 +14,7 @@ import Base: convert, promote_rule, show, showcompact,
              
 export Float65
 
+include("behavior/Reinterpret.jl")
 include("behavior/absorb.jl")
 
 include("type/Float65.jl")
