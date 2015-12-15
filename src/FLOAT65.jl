8698a6b15d03cc56920889fd632130ce71cd850c
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-15 15:48:47 -0500

    Update FLOAT65.jl

--- src/FLOAT65.jl
+++ src/FLOAT65.jl
@@ -6,11 +6,11 @@ import Base: convert, promote_rule, show, showcompact,
              zero, one,
              (==),(!=),(<),(<=),(>=),(>),isless,isequal
              (+),(-),(*),(/),(\),(%),
-             :exp, :expm1, :log, :log1p,
-             :sin, :cos, :tan, :csc, :sec, :cot,
-             :asin, :acos, :atan, :acsc, :asec, :acot,
-             :sinh, :cosh, :tanh, :csch, :sech, :coth,
-             :asinh, :acosh, :atanh, :acsch, :asech, :acoth
+             exp, expm1, log, log1p,
+             sin, cos, tan, csc, sec, cot,
+             asin, acos, atan, acsc, asec, acot,
+             sinh, cosh, tanh, csch, sech, coth,
+             asinh, acosh, atanh, acsch, asech, acoth
              
 export Float65
 
