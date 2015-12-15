99eae29e31d4968f41ee801c0ddff8394f48c96c
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-15 15:55:38 -0500

    Update elementary.jl

--- src/math/elementary.jl
+++ src/math/elementary.jl
@@ -4,6 +4,6 @@ for op in (:exp, :expm1, :log, :log1p,
            :sinh, :cosh, :tanh, :csch, :sech, :coth,
            :asinh, :acosh, :atanh, :acsch, :asech, :acoth)
     @eval begin
-        ($op)(a::Float65) = Float65( ($op)(reflect(a.fp) )
+        ($op)(a::Float65) = Float65( ($op)(reflect(a.fp)) )
     end
 end
