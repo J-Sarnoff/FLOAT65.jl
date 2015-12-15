29cac4ae15041dec21ee0b529a4a90961c46b9a9
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-15 15:31:03 -0500

    Update FLOAT65.jl

--- src/FLOAT65.jl
+++ src/FLOAT65.jl
@@ -1,14 +1,23 @@
 module FLOAT65
 
 import Base: convert, promote_rule, show, showcompact,
-             typemin, typemax, realmin, realmax,
+             typemin, typemax, realmin, realmax, sizeof,
              isinf, isnan, issubnormal, isfinite, isinteger,
-             zero, one
+             zero, one,
+             (==),(!=),(<),(<=),(>=),(>),isless,isequal
+             (+),(-),(*),(/),(\),(%),
+             :exp, :expm1, :log, :log1p,
+             :sin, :cos, :tan, :csc, :sec, :cot,
+             :asin, :acos, :atan, :acsc, :asec, :acot,
+             :sinh, :cosh, :tanh, :csch, :sech, :coth,
+             :asinh, :acosh, :atanh, :acsch, :asech, :acoth
              
 export Float65
 
 include("type/Float65.jl")
 include("behavior/absorb.jl")
+include("math/arith.jl")
+include("math/elementary.jl")
 #=
 include("basics.jl")
 include("fn1arg.jl")
