9f23341e122fbd0b6d465a3b78f8f0e353d05a58
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-16 17:08:32 -0500

    include sqrt

--- src/math/propagateState/arith.jl
+++ src/math/propagateState/arith.jl
@@ -2,6 +2,14 @@
 
 isinteger(a::Float65) = isinteger(reflect(a.fp))
 
+function sqrt{T<:Float64}(a::T)
+    value = (T)( sqrt(reflect(a.fp)) )
+    if getstate(a)
+       value = setstate(value)
+    end
+    value
+end
+
 for op in (:(+), :(-), :(*), :(/), :(\), :(%))
 
     @eval begin
