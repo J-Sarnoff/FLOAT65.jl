bf83848cd7b5b3221af121a7e4f3aee18175660c
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-16 15:37:10 -0500

    support preset PropogateState::Bool

--- src/math/elementary.jl
+++ src/math/elementary.jl
@@ -1,3 +1,24 @@
+if isdefined(:PropogateState) && PropogateState
+
+for op in (:exp, :expm1, :log, :log1p,
+           :sin, :cos, :tan, :csc, :sec, :cot,
+           :asin, :acos, :atan, :acsc, :asec, :acot,
+           :sinh, :cosh, :tanh, :csch, :sech, :coth,
+           :asinh, :acosh, :atanh, :acsch, :asech, :acoth)
+    @eval begin
+        function ($op)(a::Float65)
+            state = getstate(a)
+            value = Float65( ($op)(reflect(a.fp)) )
+            if state 
+               value = setstate(value)
+            end
+            value
+         end            
+    end
+end
+
+else # do not propogate the set state
+
 for op in (:exp, :expm1, :log, :log1p,
            :sin, :cos, :tan, :csc, :sec, :cot,
            :asin, :acos, :atan, :acsc, :asec, :acot,
@@ -6,4 +27,6 @@ for op in (:exp, :expm1, :log, :log1p,
     @eval begin
         ($op)(a::Float65) = Float65( ($op)(reflect(a.fp)) )
     end
+end
+
 end
