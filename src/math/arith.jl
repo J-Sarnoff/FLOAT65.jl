1d6b8e0c741e84151222a163fa05119d2f1a205f
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-16 16:06:04 -0500

    support autopropagation of the set state

--- src/math/arith.jl
+++ src/math/arith.jl
@@ -13,9 +13,8 @@ for op in (:(+), :(-), :(*), :(/), :(\), :(%))
     @eval begin
     
         function ($op){T<:Float65}(a::T, b::T)
-            state = getstate(a) & getstate(b)
-            
             value = (T)( ($op)(reflect(a.fp), reflect(b.fp)) )
+            state = getstate(a) & getstate(b)
             if state 
                value = setstate(value)
             end
@@ -28,9 +27,8 @@ else # do propagate the set state when at least one input is in the set state
     @eval begin
     
         function ($op){T<:Float65}(a::T, b::T)
-            state = getstate(a) | getstate(b)
-            
             value = (T)( ($op)(reflect(a.fp), reflect(b.fp)) )
+            state = getstate(a) | getstate(b)
             if state 
                value = setstate(value)
             end
@@ -60,17 +58,18 @@ for op in (:(+), :(-), :(*), :(/), :(\), :(%))
             value
          end     
          
-          
-        ($op)(a::Float65, b::Float65) = Float65( $op(reflect(a.fp), reflect(b.fp)) )
-        ($op)(a::Float65, b::Float64) = Float65( $op(reflect(a.fp), b) )
-        ($op)(a::Float64, b::Float65) = Float65( $op(a, reflect(b.fp)) )
-        
-        ($op){I<:Union{Int64,Int32}}(a::Float65, b::I) = Float65( $op(reflect(a.fp), b) )
-        ($op){I<:Union{Int64,Int32}}(a::I, b::Float65) = Float65( $op(a, reflect(b.fp)) )
-        ($op)(a::Float65, b::Bool) = Float65( $op(reflect(a.fp), convert(Float64,b)) )
-        ($op)(a::Bool, b::Float65) = Float65( $op(convert(Float64,a), reflect(b.fp)) )
-        ($op)(a::Float65, b::Real) = Float65( $op(reflect(a.fp), convert(Float64,b)) )
-        ($op)(a::Real, b::Float65) = Float65( $op(convert(Float64,a), reflect(b.fp)) )
+        ($op){I<:Union{Int64,Int32}}(a::Float65, b::I) = 
+            getstate(a) ? setstate(Float65( $op(reflect(a.fp), b) )) : Float65( $op(reflect(a.fp), b) )
+        ($op){I<:Union{Int64,Int32}}(a::I, b::Float65) = 
+            getstate(b) ? setstate(Float65( $op(a, reflect(b.fp)) )) : Float65( $op(a, reflect(b.fp)) )
+        ($op)(a::Float65, b::Bool) = 
+            getstate(a) ? setstate(Float65( $op(reflect(a.fp), convert(Float64,b)) )) : Float65( $op(reflect(a.fp), convert(Float64,b)) )
+        ($op)(a::Bool, b::Float65) = 
+            getstate(b) ? setstate(Float65( $op(convert(Float64,a), reflect(b.fp)) )) : Float65( $op(convert(Float64,a), reflect(b.fp)) )
+        ($op)(a::Float65, b::Real) = 
+            getstate(a) ? setstate(Float65( $op(reflect(a.fp), convert(Float64,b)) )) : Float65( $op(reflect(a.fp), convert(Float64,b)) )
+        ($op)(a::Real, b::Float65) = 
+            getstate(b) ? setstate(Float65( $op(convert(Float64,a), reflect(b.fp)) )) : Float65( $op(convert(Float64,a), reflect(b.fp)) )
     end
 end
 
