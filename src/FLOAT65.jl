4d44ca61b4eed86e3baffeb130d65e33ed42429b
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-16 17:09:36 -0500

    Update FLOAT65.jl

--- src/FLOAT65.jl
+++ src/FLOAT65.jl
@@ -4,13 +4,14 @@ import Base: convert, promote_rule, show, showcompact,
              typemin, typemax, realmin, realmax, sizeof,
              isinf, isnan, issubnormal, isfinite, isinteger,
              zero, one,
-             (==),(!=),(<),(<=),(>=),(>),isless,isequal,
+             (==),(!=),(<),(<=),(>=),(>), isless, isequal,
              (+),(-),(*),(/),(\),(%), sqrt,
              exp, expm1, log, log1p,
              sin, cos, tan, csc, sec, cot,
              asin, acos, atan, acsc, asec, acot,
              sinh, cosh, tanh, csch, sech, coth,
-             asinh, acosh, atanh, acsch, asech, acoth
+             asinh, acosh, atanh, acsch, asech, acoth,
+             (^)
              
 export Float65, setstate, clearstate, getstate
 
