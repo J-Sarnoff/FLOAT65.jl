a9e6d709e4f6891e23791a0d799b31ea21040203
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-16 17:07:03 -0500

    include sqrt

--- src/math/assignState/arith.jl
+++ src/math/assignState/arith.jl
@@ -2,6 +2,8 @@
 
 isinteger(a::Float65) = isinteger(reflect(a.fp))
 
+sqrt{T<:Float65}(a::T) = (T)( sqrt(reflect(a.fp)) )
+
 for op in (:(+), :(-), :(*), :(/), :(\), :(%))
     @eval begin
         ($op){T<:Float65}(a::T, b::Float65) = (T)( $op(reflect(a.fp), reflect(b.fp)) )
