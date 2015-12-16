6c68ff5f1ca21cffcea77ca933a32367de9c931c
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-15 19:06:32 -0500

    Update Float65.jl

--- src/type/Float65.jl
+++ src/type/Float65.jl
@@ -8,16 +8,14 @@ end
 
 convert(::Type{Float64}, x::Float65) = reflect(x.fp)
 convert(::Type{Float65}, x::Float64) = Float65(project(x))
-convert(::Type{Float65}, x::Bool) = Float65(convert(Float64,x))
-convert(::Type{Float65}, x::Real) = Float65(convert(Float64,x))
-
+for T in (:Float32, :Float16, :Int64, :Int32, :Int16, :Bool, :Real)
+    @eval convert(::Type{Float65}, x::$T) = Float65(convert(Float64,x))
+end
 
 promote_rule(::Type{Float64}, ::Type{Float65}) = Float65
-promote_rule(::Type{Int64}, ::Type{Float65}) = Float65
-promote_rule(::Type{Int32}, ::Type{Float65}) = Float65
-promote_rule(::Type{Bool}, ::Type{Float65}) = Float65
-promote_rule(::Type{Real}, ::Type{Float65}) = Float65
-
+for T in (:Float64, :Float32, :Float16, :Int64, :Int32, Int16, :Bool, :Real)
+   @eval promote_rule(::Type{Float65}, ::Type{$T}) = Float65
+end   
 
 show(io::IO, x::Float65) = show(io, convert(Float64,x))
 
