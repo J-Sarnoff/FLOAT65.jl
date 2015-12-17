e525a25c644a9a775db7ccd55f29eb646a83b796
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-16 20:46:18 -0500

    Update io.jl

--- src/type/io.jl
+++ src/type/io.jl
@@ -1,4 +1,4 @@
-const NaN         = "NaN"
+const NaNValue    = "NaN"
 
 const InfValue    = "∞"
 const PosInfValue = "+∞" 
@@ -10,5 +10,50 @@ const HugeValue = "⬬"
 const TinyName  = "TINY"
 const HugeName  = "HUGE"
 
+const Exactly   = "⌁"
+const Inexactly = "~"
 
-show(io::IO, x::Float65) = show(io, convert(Float64,x))
+function show(io::IO, x::Float65)
+
+    state = getstate(x)
+    fp = convert(Float64, x)
+    
+    if AsTiny(F) <= fp <= AsHuge(F)
+       s = prepshow(fp, state)
+    elseif fp == 0.0
+       s = prepshow0(fp, state)
+    elseif isfinite(fp)
+       if fp <= AsTiny(F)
+          s = prepshowTiny(fp, state)
+       else
+          s = prepshowHuge(fp, state)
+       end
+    elseif isinf(fp)  # ±Inf or NaN
+       s = prepshowInf(fp,state)
+    else
+       s = prepshowNaN(fp,state)
+    end
+
+    print(io, s)
+end
+
+@inline prepshowState(state::Bool) = state ? Inexactly : Exactly
+
+function prepshow(fp::Float64, state::Bool)
+   string(fp, prepshowState(state))
+end
+function prepshow0(fp::Float64, state::Bool)
+   string(fp, prepshowState(state))
+end
+function prepshowTiny(fp::Float64, state::Bool)
+   string( (signbit(fp) ? "-" : "+"), TinyValue )
+end
+function prepshowHuge(fp::Float64, state::Bool)
+   string( (signbit(fp) ? "-" : "+"), HugeValue )
+end
+function prepshowInf(fp::Float64, state::Bool)
+   string( (signbit(fp) ? "-" : "+"), InfValue )
+end
+function prepshowNaN(fp::Float64, state::Bool)
+   string( (signbit(fp) ? "-" : "+"), NaNValue )
+end
