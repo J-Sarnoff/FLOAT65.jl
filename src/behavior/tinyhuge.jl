b0eeee8a2086950996d9d6451d94b5bdd9c2b052
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-17 18:52:15 -0500

    Update tinyhuge.jl

--- src/behavior/tinyhuge.jl
+++ src/behavior/tinyhuge.jl
@@ -20,7 +20,7 @@ AsTiny(::Type{Float64}) = 1.1933345169920327e-153 # first inadmissible small mag
 AsHuge(::Type{Float64}) = 8.379879956214127e152   # first inadmissible large magnitude
                                                   # 1/AsTiny == AsHuge, 1/AsHuge == AsTiny
 Tiny(::Type{Float64})   = 2.983336292480083e-154  # collective value representing all smallest normal magnitudes
-Huge(::Type{Float64})   = 3.35195198248565e+153   # collective value representing all largest  normal magnitudes
+Huge(::Type{Float64})   = 3.3519519824856493e153  # collective value representing all largest  normal magnitudes
                                                   # 1/Tiny == Huge, 1/Huge == Tiny
 # above, reinterpreted before projection
 Bias(::Type{UInt64})   = 0x00000000000001ff
@@ -33,7 +33,7 @@ Huge(::Type{UInt64})   = 0x5fd0000000000001
 AsTinyProjected(::Type{Float64}) = 1.7800590868057605e-307
 AsHugeProjected(::Type{Float64}) = 0.12500000000000006
   TinyProjected(::Type{Float64}) = 4.450147717014403e-308
-  HugeProjected(::Type{Float64}) = 0.5000000000000001
+  HugeProjected(::Type{Float64}) = 0.5000000000000000
 AsTinyProjected(::Type{UInt64})  = 0x003ffffffffffffd
 AsHugeProjected(::Type{UInt64})  = 0x3fc0000000000002
   TinyProjected(::Type{UInt64})  = 0x0020000000000000
