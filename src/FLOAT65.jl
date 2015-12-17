1def9bc62606c0dde5870c2f4386e3bb448a2624
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-17 06:59:06 -0500

    Update FLOAT65.jl

--- src/FLOAT65.jl
+++ src/FLOAT65.jl
@@ -3,7 +3,7 @@ module FLOAT65
 import Base: convert, promote_rule, show, showcompact,
              typemin, typemax, realmin, realmax, sizeof,
              isinf, isnan, issubnormal, isfinite, isinteger,
-             zero, one, sign, signbit, abs,
+             zero, one, sign, signbit, abs, eps,
              (==),(!=),(<),(<=),(>=),(>), isless, isequal,
              (+),(-),(*),(/),(\),(%), sqrt,
              exp, expm1, log, log1p,
