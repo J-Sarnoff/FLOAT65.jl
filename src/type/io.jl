544a53833ec13a6d74c9f002b877771d00c59e88
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-17 07:32:05 -0500

    Update io.jl

--- src/type/io.jl
+++ src/type/io.jl
@@ -10,7 +10,7 @@ const HugeValue = "⬬"
 const TinyName  = "TINY"
 const HugeName  = "HUGE"
 
-const Exactly   = "⌁"
+const Exactly   = "𝆘 #""⌁"
 const Inexactly = "~"
 
 function show(io::IO, x::Float65)
