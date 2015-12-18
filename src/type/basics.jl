46e63a572739ea2056bcf49274fdf473e799b947
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-18 09:10:05 -0500

    Update basics.jl

--- src/type/basics.jl
+++ src/type/basics.jl
@@ -1,10 +1,5 @@
 # these do not require decoding to work correctly
 
-issubnormal(a::Float65) = false
-for op in (:sign, :signbit, :isinf, :isnan, :isfinite)
-    @eval ($op)(a::Float65) = ($op)(a.fp)
-end
-
 typemin(::Type{Float65}) = typemin(Float64)
 typemax(::Type{Float65}) = typemin(Float64)
 typemin(x::Float65) = typemin(Float65)
