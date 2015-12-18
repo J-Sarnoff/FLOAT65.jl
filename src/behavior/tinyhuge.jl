a253bc2a5a5a4fc67bc14b77fca5440168600086
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-18 09:08:38 -0500

    simplify, show how values are determined

--- src/behavior/tinyhuge.jl
+++ src/behavior/tinyhuge.jl
@@ -24,36 +24,30 @@
 # projection takes domain(Float64) onto  halfdomain(Float64)
 @inline isProjectedTiny{T<:AbstractFloat}(x::T) = (abs(x) <= TinyProjected(T)) & nonzero(x)
 @inline isProjectedHuge{T<:AbstractFloat}(x::T) = (abs(x) >= HugeProjected(T)) & noninf(x)
-# projection takes domain(Float64) onto  halfdomain(Float64)
+# stateful takes projection into one of two coextant, equivalued halfdomain(Float64) bistablities
 @inline isStatefulTiny{T<:AbstractFloat}(x::T) = (abs(x) <= TinyProjected(T)) & nonzero(x)
 @inline isStatefulHuge{T<:AbstractFloat}(x::T) = (abs(x) >= HugeProjected(T)) & noninf(x)
 
 
-
+# Bias is used in projective and reflective mappings (projection and reflection behave as dual ops)
 Bias(::Type{Float64}) = 511                     # exponent_bias(Float64) >> 1
-Bias(::Type{UInt64})  = 0x00000000000001ff      # for use with other UInt64 bitforms
+Bias(::Type{UInt64})  = 0x00000000000001ff      # use with bitops
 
-# Tiny and Huge, valuations to be used as proxies for their respective collective intent.
+# Tiny and Huge, assigned
 #
-AsTiny(::Type{Float64}) = 1.1933345169920327e-153 # first inadmissible small magnitude & all smallest
-AsHuge(::Type{Float64}) = 8.379879956214127e152   # first inadmissible large magnitude
-                                                  # 1/AsTiny == AsHuge, 1/AsHuge == AsTiny
-Tiny(::Type{Float64})   = 2.983336292480083e-154  # collective value representing all smallest normal magnitudes
-Huge(::Type{Float64})   = 3.3519519824856493e153  # collective value representing all largest  normal magnitudes
-                                                  # 1/Tiny == Huge, 1/Huge == Tiny
-# above, reinterpreted before projection
-Bias(::Type{UInt64})   = 0x00000000000001ff
-AsTiny(::Type{UInt64}) = 0x202ffffffffffffd
-AsHuge(::Type{UInt64}) = 0x5fb0000000000002
-Tiny(::Type{UInt64})   = 0x2010000000000000
-Huge(::Type{UInt64})   = 0x5fd0000000000001
+Tiny(::Type{Float64}) = 2.983336292480083e-154  # collective value representing all smallest normal magnitudes
+Huge(::Type{Float64}) = 3.3519519824856493e153  # collective value representing all largest  normal magnitudes
+Tiny(::Type{UInt64})  = 0x2010000000000000      # use with bitops (Tiny as largest  inadmissble small magnitude)
+Huge(::Type{UInt64})  = 0x5fd0000000000000      # use with bitops (Huge as smallest inadmissble large magnitude)
 
-# above, after projection
-AsTinyProjected(::Type{Float64}) = 1.7800590868057605e-307
-AsHugeProjected(::Type{Float64}) = 0.12500000000000006
-  TinyProjected(::Type{Float64}) = 4.450147717014403e-308
-  HugeProjected(::Type{Float64}) = 0.5000000000000000
-AsTinyProjected(::Type{UInt64})  = 0x003ffffffffffffd
-AsHugeProjected(::Type{UInt64})  = 0x3fc0000000000002
-  TinyProjected(::Type{UInt64})  = 0x0020000000000000
-  HugeProjected(::Type{UInt64})  = 0x3fe0000000000001
+# Tiny and Huge, projected
+#
+TinyProjected(::Type{Float64}) = 4.450147717014403e-308 # ldexp(0.5,-1020) == frexp(pushout(Tiny(Float64))
+HugeProjected(::Type{Float64}) = 0.5000000000000000     # ldexp(0.5,+0)    == frexp(pushout(Huge(Float64))
+TinyStateful( ::Type{Float64}) = 8.0000000000000000     # ldexp(0.5,+4)    == set_ebit(TinyProjected(Float64))
+HugeStateful( ::Type{Float64}) = 8.98846567431158e+307  # ldexp(0.5,+1024) == set_ebit(HugeProjected(Float64))
+                                                        #            use with bitops
+TinyProjected(::Type{UInt64})  = 0x0020000000000000     # reinterpret(UInt64, TinyProjected(Float64))
+HugeProjected(::Type{UInt64})  = 0x3fe0000000000000     # reinterpret(UInt64, HugeProjected(Float64))
+TinyStateful( ::Type{UInt64})  = 0x4020000000000000     # reinterpret(UInt64, TinyStateful(Float64))
+HugeStateful( ::Type{UInt64})  = 0x7fe0000000000000     # reinterpret(UInt64, HugeStateful(Float64))
