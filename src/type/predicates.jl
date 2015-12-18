3011b47214293b302ce7a081c044c8dc142a608d
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-18 09:17:29 -0500

    Update predicates.jl

--- src/type/predicates.jl
+++ src/type/predicates.jl
@@ -1,9 +1,10 @@
-@inline nonzero{T<:AbstractFloat}(x::T) = (zero(T) != x)
-@inline noninf{T<:AbstractFloat}(x::T)  = (!isinf(x))
+@inline isnotzero{T<:AbstractFloat}(x::T) = (zero(T) != x)
+@inline isnotinf{T<:AbstractFloat}(x::T)  = (!isinf(x))
+@inline isnormal{T<:AbstractFloat}(x::T)  = (!isnan(x) & !isinf(x) & !issubnormal(x))
 
 # these do not require decoding to work correctly
 
 issubnormal(a::Float65) = false
-for op in (:sign, :signbit, :isinf, :isnan, :isfinite)
+for op in (:sign, :signbit, :isinf, :isnan, :isfinite, :isinteger)
     @eval ($op)(a::Float65) = ($op)(a.fp)
 end
