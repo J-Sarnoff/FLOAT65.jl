2b3e7418085b87378a50fcb48ef6d25ec138383f
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-18 23:09:38 -0500

    Update predicates.jl

--- src/type/predicates.jl
+++ src/type/predicates.jl
@@ -1,8 +1,8 @@
-@inline isnotfinite{T<:AbstractFloat}(x::T)  = (isinf(x) | isnan(x)) 
-@inline isnormal{T<:AbstractFloat}(x::T)     = !(isinf(x) | isnan(x) | issubnormal(x))
+@inline iszero{T<:AbstractFloat}(x::T)       = (zero(T) == x)
 @inline isnotzero{T<:AbstractFloat}(x::T)    = (zero(T) != x)
+@inline isnormal{T<:AbstractFloat}(x::T)     = !(isinf(x) | isnan(x) | issubnormal(x))
 @inline isnotinf{T<:AbstractFloat}(x::T)     = (!isinf(x))
-
+@inline isnotfinite{T<:AbstractFloat}(x::T)  = (isinf(x) | isnan(x)) 
 
 # these do not require decoding to work correctly
 
